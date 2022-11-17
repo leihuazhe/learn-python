8b1ae4f3a906637a5b9598acc2857eae175f9b94
leihuazhe <leihuazhe@sensorsdata.cn>
2022-11-17 19:16:10 +0800

    Reserved, time:

--- src/alluvial/elementary/anon_func_10.py
+++ src/alluvial/elementary/anon_func_10.py
@@ -10,8 +10,20 @@ square = lambda x: x ** 2
 
 def square2(x):
     return x ** 2
+
+
 #
 print(square(4))
 print(square(100))
 #
 print(square2(4))
+
+# 在列表内部使用,def不能
+print([(lambda x: x * x)(x) for x in range(10)])
+# lambda 可以被用作某些函数的参数，而常规函数 def 也不能
+l = [(1, 20), (3, 0), (9, 10), (2, -1)]
+l.sort(key=lambda x: x[1])
+print(l)
+# 常规函数 def 必须通过其函数名被调用，因此必须首先被定义。
+# 但是作为一个表达式的 lambda，返回的函数对象就不需要名字了。
+

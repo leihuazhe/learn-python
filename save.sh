18fe97304ef6771471fe6ff4305c9c843f8f0abc
leihz <leihuazhe@gmail.com>
2022-11-22 22:33:11 +0800

    Reserved, time: Tue Nov 22 22:33:11     2022

--- save.sh
+++ save.sh
@@ -2,6 +2,6 @@ git pull origin master
 git pull giteee master  || true
 date +"%Y-%m-%d %T"
 git add .
-git commit -am "Reserved, time: $date"
+git commit -am "Reserved, time: $(date)"
 git push origin master
 git push giteee master || true
